1c857158db945f17e562a476358f2192c339d912
Lucas Pagliero <pagliero.lucas.g@gmail.com>
2023-11-08 20:03:02 +0000

    primera subida

---
+++ ProyectoPerros/Perro.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPerros
+{
+    internal class Perro
+    {
+
+        //CONSTRUCTOR
+
+        public Perro(string raza, string sexo, string color) { 
+        
+            this.raza = raza;
+            this.sexo = sexo;
+            this.color = color;
+        }
+        //Raza
+        //Sexo
+        //Edad
+        //Color
+        //Compañia
+
+        private string raza;
+
+        private string sexo;
+
+        private int edad;
+
+        private string color;
+
+        public int comp;
+
+        //PROPIEDADES
+
+        public string Raza { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Color { get; set; }
+
+        public int Comp { get; set; }
+
+        public int Edad
+        { get { return edad; } set {  edad = value; } }
+
+
+        //METODOS
+
+        //METODO OBTENCION DE DATOS
+
+
+
+
+        //METODO EDAD
+
+        public void vejez()
+        {
+            if(edad > 10)
+            {
+                Console.WriteLine("Edad avanzada.");
+            }
+            else
+            {
+                Console.WriteLine("Joven.");
+            }
+
+        }
+
+        //METODO COMPAÑIA
+
+        public void compa()
+        { if(comp < 2) 
+            {
+                Console.WriteLine("El perro podria necesitar compañia");
+            }
+        else
+            {
+                Console.WriteLine("No necesita mas compañia");
+
+            }
+
+            Console.WriteLine("---------------");
+
+            Console.ReadKey();
+        }
+        //METODO FICHA
+
+        public void ficha()
+        {
+    
+
+            Console.WriteLine("Raza: "+ raza);
+            Console.WriteLine("Sexo "+ sexo);
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Edad: " + edad);
+
+         
+
+           
+        }
+
+    }
+}
---
+++ ProyectoPerros/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPerros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Perro perro_1 = new Perro("Border Collie", "Macho","Blanco y negro");
+            Perro perro_2 = new Perro("Pastor Aleman", "Macho", "Negro");
+            Perro perro_3 = new Perro("Dogo", "Hembra", "Blanca");
+            Perro perro_4= new Perro("Chihuahua", "Macho", "Marron");
+
+
+            Console.WriteLine("Ingrese la edad del perro 1");
+            perro_1.Edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese con cuantos perros convive: ");
+            perro_1.comp = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Ingrese la edad del perro 2");
+            perro_2.Edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese con cuantos perros convive: ");
+            perro_2.comp = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Ingrese la edad del perro 3");
+            perro_3.Edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese con cuantos perros convive: ");
+            perro_3.comp = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Ingrese la edad del perro 4");
+            perro_4.Edad = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese con cuantos perros convive: ");
+            perro_4.comp = int.Parse(Console.ReadLine());
+
+            perro_1.ficha();
+            perro_1.vejez();
+            perro_1.compa();
+
+            perro_2.ficha();
+            perro_2.vejez();
+            perro_2.compa();
+
+            perro_3.ficha();
+            perro_3.vejez();
+            perro_3.compa();
+
+            perro_4.ficha();
+            perro_4.vejez();
+            perro_4.compa();
+        }
+    }
+}
